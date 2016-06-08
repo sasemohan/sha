cc7b8e62dce6d668c0d186593eb8b3b76de11586
sharmi1902 <sharmila.cs13@bitsathy.ac.in>
2016-06-08 11:58:00 +0530

    Create 20.java

---
+++ 20.java
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Rextester
+{
+    public class Program
+    {
+        int fact;
+
+        
+        public static void Main(string[] args)
+        {
+            string text1 = "500";
+            string text2 = "10";
+            int num1 = int.Parse(text1);
+            int num2 = int.Parse(text2);
+            Console.WriteLine(num1*num2);
+            
+
+            
+
+           
+ 
+          
+        }
+    }
+}
